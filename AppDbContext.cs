f99032e8931ec95bc8a9ae424b76540b3cf9dcad
Kevin Membreno <83984148+JustYourFriendlyNeighborhoodProgrammer@users.noreply.github.com>
2024-02-28 23:25:12 -0600

    ASP.NET Core Web App (Razor Pages)

---
+++ AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using CS451_Team_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CS451_Team_Project
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
