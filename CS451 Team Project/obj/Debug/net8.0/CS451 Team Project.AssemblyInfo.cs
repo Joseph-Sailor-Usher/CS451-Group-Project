9b2fe327dc00c2639244be0e23640390712e65d1
Joseph Sailor Usher <100800088+Joseph-Sailor-Usher@users.noreply.github.com>
2024-03-14 11:50:34 -0500

    Reorganizaed the database

--- CS451 Team Project/obj/Debug/net8.0/CS451 Team Project.AssemblyInfo.cs
+++ CS451 Team Project/obj/Debug/net8.0/CS451 Team Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CS451 Team Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce76021fb7e5ba4ced747175e534c76371631fee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2b8fdb404b24e3b6714a330cc8075c96664c720")]
 [assembly: System.Reflection.AssemblyProductAttribute("CS451 Team Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CS451 Team Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
